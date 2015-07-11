9b32e5e0a9745ce84aaaad64d7203aaad2017051
Sam Carlton <thatguy@samuelcarlton.com>
2015-07-11 10:11:47 -0500

    Updated debuggin features

--- config.php
+++ config.php
@@ -1,5 +1,23 @@
 <?php
 	
+	if( $_GET['debug'] ){
+	
+		ini_set('display_errors',1);
+		ini_set('display_startup_errors',1);
+		error_reporting(-1);
+		
+	}
+	
+	function debug( $thing ) {
+		$output_data = $thing;
+		if( !isset( $thing ) ) $output_data = 'That\'s not a set value';
+		if( $thing === null ) $output_data = 'He\'s Null Jim';
+		if( $thing === false ) $output_data = 'Returned False';
+		if( $thing === 0 ) $output_data = 'Returned Zero';
+		ob_start(); ?><pre><?php var_dump($thing); ?></pre><?php $output = ob_get_clean();
+		echo $output;
+	}
+	
 	$whitelist = array(
 		'127.0.0.1',
 		'::1'
