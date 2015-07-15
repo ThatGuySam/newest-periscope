9a5c0c5e7d99740d8bf5b06cb18ef347e602a8c1
Sam Carlton <thatguy@samuelcarlton.com>
2015-07-14 20:00:26 -0500

    Added better error handling

--- index.php
+++ index.php
@@ -34,42 +34,78 @@ if($link == null) {
 		"count"					=> intval(POSTS_COUNT),
 	));
 	
-	
-	foreach( $content as $tweet_object ){
+	if( empty( $content->errors ) ){//All is dandy
 		
-		$urls = $tweet_object->entities->urls;//Get URLS
-		//$debug = $urls;
-		foreach( $urls as $url ){
-			//$debug = $url->expanded_url;
-			if (strpos($url->display_url,'periscope.tv') !== false)//Find periscope link
-				$link = $url->expanded_url; break;//Use that
+		
+		foreach( $content as $tweet_object ){
+			
+			$urls = $tweet_object->entities->urls;//Get URLS
+			foreach( $urls as $url ){
+				//$debug = $url->expanded_url;
+				if (strpos($url->display_url,'periscope.tv') !== false)//Find periscope link
+					$link = $url->expanded_url; break;//Use that
+			}
+			
+			if( $link !== null ) break;
+			
 		}
 		
-		if( $link !== null ) break;
+		if( $link !== null ) {
+			
+			$cache->set( $username , $link , CACHE_TIME );
+			
+		} else {
+			echo "No Periscopes found in the last ".POSTS_COUNT." tweets";
+		}
 		
-	}
-	
-	$cache->set( $username , $link , CACHE_TIME );
+		
+	} else {//it's GDFR
+		
+		//So what's going on? What are the error messages
+		
+		$error_codes = array();
+		
+		foreach( $content->errors as $error ){
+			
+			array_push($error_codes, $error->code );
+			
+			$cache->set( 'er_'.$username.'_'.$error->code , $error , CACHE_TIME );
+			
+			debug( $error );
+		}
+		
+		//34: Bad request
+		//88: Rate Limit Exceeded
+		
+		//Let's try a few things
+		
 /*
-	if( $link ){
-		
-	}
+		if( $cache->isExisting($username) ){//Use cached version
+			
+			debug( $cache->getInfo($username) );
+			
+			//$cache->get($username)
+						
+		}
 */
-	
-	if( $link !== null ) {
 		
-		$cache->set( $username , $link , CACHE_TIME );
 		
-	} else {
-		echo "No Periscopes found in the last ".POSTS_COUNT." tweets";
+		
+		//No good? Time to analyze
+		
+		echo "Something is wrong...";
+		
+		//Log/Display Errors
+		
 	}
 		
 } else {
 	//echo "Used Cache <br><br>";
 }
 
-if ( headers_sent() ) exit;//Already headers? Then don't redirect
 
+
+if ( headers_sent() ) exit;//Already headers? Then don't redirect
 
 header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
 header('Expires: Sat, 26 Jul 1997 05:00:00 GMT'); // past date to encourage expiring immediately
